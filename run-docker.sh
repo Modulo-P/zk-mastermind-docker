6291452bc76e55f77a42fa92407e392e36faed6e
Juan Salvador Magán Valero <jmaganvalero@gmail.com>
2024-04-20 20:50:36 +0000

    doc: add funding tips

--- run-docker.sh
+++ run-docker.sh
@@ -3,6 +3,8 @@
 
 set -e
 
+echo "Bootstraping ZK Mastermind solution"
+
 docker compose down
 
 cd backend
@@ -23,10 +25,24 @@ cd hydra/credentials
 
 ./generate-credentials.sh
 
-cd ../..
+cd ..
+cd ..
 
 docker compose build --pull backend
 
 docker compose build --pull frontend
 
 docker compose up -d  --pull always
+
+echo "
+Please add funds to this address:
+
+Cardano funds 1 address (~1000 tADA): $(cat hydra/credentials/cardano-funds-1.addr)
+Cardano funds 2 address (~100 tADA): $(cat hydra/credentials/cardano-funds-2.addr)
+Cardano node 1 address (~1000 tADA): $(cat hydra/credentials/cardano-node-1.addr)
+Cardano node 2 address (~100 tADA): $(cat hydra/credentials/cardano-node-2.addr)
+
+Once the funds are in the addresses you can check if the Hydra head is open:
+
+docker compose logs -f -n 1000 backend 
+"

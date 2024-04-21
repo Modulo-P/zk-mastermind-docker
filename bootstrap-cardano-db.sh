e6393e615363ed7fc0e3e1e21be839c53d4b925a
Juan Salvador Magán Valero <jmaganvalero@gmail.com>
2024-04-21 09:10:11 +0200

    feat: remove debug flag

--- bootstrap-cardano-db.sh
+++ bootstrap-cardano-db.sh
@@ -5,5 +5,5 @@ GENESIS_VERIFICATION_KEY=$(wget -q -O - https://raw.githubusercontent.com/input-
 export AGGREGATOR_ENDPOINT=https://aggregator.release-preprod.api.mithril.network/aggregator
 
 if [ ! -d "hydra/db" ]; then
-  docker run --rm -it -e NETWORK=$NETWORK -e GENESIS_VERIFICATION_KEY=$GENESIS_VERIFICATION_KEY -e AGGREGATOR_ENDPOINT=$AGGREGATOR_ENDPOINT --name='mithril-client' -v $(pwd):/app/data -u $(id -u) ghcr.io/input-output-hk/mithril-client:latest -vvv cardano-db download --download-dir /app/data/hydra latest 
+  docker run --rm -it -e NETWORK=$NETWORK -e GENESIS_VERIFICATION_KEY=$GENESIS_VERIFICATION_KEY -e AGGREGATOR_ENDPOINT=$AGGREGATOR_ENDPOINT --name='mithril-client' -v $(pwd):/app/data -u $(id -u) ghcr.io/input-output-hk/mithril-client:latest cardano-db download --download-dir /app/data/hydra latest 
 fi
